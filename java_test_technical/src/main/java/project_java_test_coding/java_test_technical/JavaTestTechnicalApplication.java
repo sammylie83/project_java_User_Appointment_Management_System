65b66e3b10c3a30910cbfc7252392824a288f381
sammylie83@gmail.com <sammylie83@gmail.com>
2025-09-10 10:44:40 +0700

    update code 100902025

---
+++ java_test_technical/src/main/java/project_java_test_coding/java_test_technical/JavaTestTechnicalApplication.java
@@ -0,0 +1,13 @@
+package project_java_test_coding.java_test_technical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestTechnicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTestTechnicalApplication.class, args);
+	}
+
+}
---
+++ java_test_technical/src/test/java/project_java_test_coding/java_test_technical/JavaTestTechnicalApplicationTests.java
@@ -0,0 +1,13 @@
+package project_java_test_coding.java_test_technical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestTechnicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
